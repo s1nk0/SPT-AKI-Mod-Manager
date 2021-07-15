ec6de446aae3a1692d275c14207350c15f72ca8b
s1nk0 <mfallejo@gmail.com>
2021-07-15 00:58:01 -0400

    Add files via upload

---
+++ SPT-AKI Mod Manager/Dialog1.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Windows.Forms
+Imports System.Net
+Imports System.Diagnostics
+
+Public Class Dialog1
+
+    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.Close()
+    End Sub
+
+    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Close()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Dialog1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Using client As New WebClient
+            Dim value As String = client.DownloadString("https://pastebin.com/raw/Xi2ZifqD")
+            TextBox1.Text = value
+        End Using
+    End Sub
+End Class
---
+++ SPT-AKI Mod Manager/Dialog2.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Windows.Forms
+
+Public Class Dialog2
+
+    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.OK
+        My.Computer.FileSystem.WriteAllText(My.Computer.FileSystem.CurrentDirectory + "\paths\gameFolder.txt", TextBox1.Text, False)
+        My.Computer.FileSystem.WriteAllText(My.Computer.FileSystem.CurrentDirectory + "\paths\modsFolder.txt", TextBox2.Text, False)
+        'My.Settings.modFolder = TextBox1.Text
+        'My.Settings.modRepo = TextBox2.Text
+        'My.Settings.Save()
+        Me.Close()
+    End Sub
+
+    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        TextBox1.Clear()
+        TextBox2.Clear()
+        Me.Close()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If FolderBrowserDialog1.ShowDialog = DialogResult.OK Then
+            TextBox1.Text = FolderBrowserDialog1.SelectedPath
+        End If
+    End Sub
+
+    Private Sub Dialog2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Form1.loadPaths()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If FolderBrowserDialog1.ShowDialog = DialogResult.OK Then
+            TextBox2.Text = FolderBrowserDialog1.SelectedPath
+
+        End If
+    End Sub
+End Class
